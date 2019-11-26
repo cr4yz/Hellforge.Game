526d98e3a41eb42dd9849b424dfea5453693152f
Crayz <jacob.ee92@gmail.com>
2019-11-25 21:34:57 -0600

    allocate skill points

--- Hellforge.Game/Assets/Scripts/Hellforge.Game/UI/Character/SkillIconRenderer.cs
+++ Hellforge.Game/Assets/Scripts/Hellforge.Game/UI/Character/SkillIconRenderer.cs
@@ -3,10 +3,11 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using Hellforge.Game.World;
+using Hellforge.Core.Entities;
 
 namespace Hellforge.Game.UI
 {
-    public class SkillIconRenderer : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    public class SkillIconRenderer : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler
     {
 
         [SerializeField]
@@ -14,19 +15,55 @@ namespace Hellforge.Game.UI
         [SerializeField]
         private SkillLabelRenderer _skillLabel;
         private string _skillName;
-        private string _skillClass;
 
         public void Render(SkillEntry skill)
         {
-            var rank = GameWorld.Instance.Character.Allocations.GetPoints(Core.Entities.AllocationType.Skill, skill.Name);
-            _rankText.text = $"{rank}/{skill.MaxRank}";
+            var currentRank = GameWorld.Instance.Character.Allocations.GetPoints(AllocationType.Skill, skill.Name);
+            _rankText.text = $"{currentRank}/{skill.MaxRank}";
             _skillName = skill.Name;
-            _skillClass = skill.Class;
         }
 
         public void OnPointerEnter(PointerEventData eventData)
         {
-            var skillData = D4Data.Instance.Hellforge.GameData.Skills.First(x => x.Name == _skillName && x.Class == _skillClass);
+            UpdateSkillLabel();
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            _skillLabel.gameObject.SetActive(false);
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            var skillData = D4Data.Instance.Hellforge.GameData.Skills.First(x => x.Name == _skillName);
+            var currentRank = GameWorld.Instance.Character.Allocations.GetPoints(AllocationType.Skill, _skillName);
+            var increment = 1;
+
+            if(eventData.button == PointerEventData.InputButton.Right)
+            {
+                if(currentRank == 0)
+                {
+                    return;
+                }
+                increment = - 1;
+            }
+            else
+            {
+                if(currentRank == skillData.MaxRank)
+                {
+                    return;
+                }
+            }
+
+            GameWorld.Instance.Character.Allocations.IncrementAllocation(AllocationType.Skill, _skillName, increment);
+
+            UpdateSkillLabel();
+            Render(skillData);
+        }
+
+        private void UpdateSkillLabel()
+        {
+            var skillData = D4Data.Instance.Hellforge.GameData.Skills.First(x => x.Name == _skillName);
             var rt = _skillLabel.GetComponent<RectTransform>();
             var oldParent = _skillLabel.transform.parent;
             _skillLabel.transform.SetParent(transform);
@@ -34,11 +71,6 @@ namespace Hellforge.Game.UI
             _skillLabel.transform.SetParent(oldParent, true);
             _skillLabel.gameObject.SetActive(true);
             _skillLabel.Render(skillData);
-        }
-
-        public void OnPointerExit(PointerEventData eventData)
-        {
-            _skillLabel.gameObject.SetActive(false);
         }
 
     }
