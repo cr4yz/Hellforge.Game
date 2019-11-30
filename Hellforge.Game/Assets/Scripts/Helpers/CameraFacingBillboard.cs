c77693121ea2a34e28d66a6c43a52d3bff5bfd2f
Crayz <jacob.ee92@gmail.com>
2019-11-30 12:08:30 -0600

    start on overhead health bar

---
+++ Hellforge.Game/Assets/Scripts/Helpers/CameraFacingBillboard.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFacingBillboard : MonoBehaviour
+{
+    public Camera m_Camera;
+
+    void Start()
+    {
+        if(m_Camera == null)
+        {
+            m_Camera = Camera.main;
+        }
+    }
+
+    //Orient the camera after all movement is completed this frame to avoid jittering
+    void LateUpdate()
+    {
+        transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.forward,
+            m_Camera.transform.rotation * Vector3.up);
+    }
+}
